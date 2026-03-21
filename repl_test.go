6490d4ed77fd196601c8f6f72fe909a06bc62f10
Jordan <jr.hunk2@gmail.com>
2026-03-21 09:45:50 -0700

    add cleanInput and tests

---
+++ repl_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+	  input    string
+	  expected []string
+        }{
+	  {
+		input:    "Charmander Bulbasaur PIKACHU",
+		expected: []string{"charmander", "bulbasaur", "pikachu"},
+	  },
+	  {
+		input:    "  charmander  ",
+		expected: []string{"charmander"},
+	  },
+	  {
+		input:    "",
+		expected: []string{},
+	  },
+        }
+
+for _, c := range cases {
+	actual := cleanInput(c.input)
+	if len(actual) != len(c.expected) {
+		t.Errorf("...")
+		continue
+	}
+	for i := range actual {
+		word := actual[i]
+		expectedWord := c.expected[i]
+		if word != expectedWord {
+			t.Errorf("...")
+		}
+	}
+    }
+}
